ce438577f91b1583aba81d213d6d7c88844f7739
15238050020 <18839609512@163.com>
2021-01-19 17:36:08 +0800

    新加代码

--- untitled/src/Test01.java
+++ untitled/src/Test01.java
@@ -5,4 +5,12 @@
  * @Describe:
  */
 public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(123456);
+        for (int i = 0; i < 100; i++) {
+
+        }
+    }
+
+
 }
