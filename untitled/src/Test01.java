bfc8155681b2f49c603b7c6c59e5fe7bb4034ce1
15238050020 <18839609512@163.com>
2021-01-20 10:58:09 +0800

    修改业务

--- untitled/src/Test01.java
+++ untitled/src/Test01.java
@@ -8,7 +8,7 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println(123456);
         for (int i = 0; i < 100; i++) {
-
+            System.out.println(i);
         }
         System.out.println("aaa");
 
